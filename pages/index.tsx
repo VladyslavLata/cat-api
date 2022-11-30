fa41a6582b5ad871a72a308775be22d79fc6f5db
VladyslavLata <vladlatahtc@gmail.com>
2022-11-30 18:24:58 +0200

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useMediaQuery } from 'usehooks-ts'
 
-export default function Home() {
+
+
+export default function  Home() {
+ const mediaTab = useMediaQuery('(min-width: 768px)')
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +16,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+     
       <main className={styles.main}>
+     
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -20,12 +27,14 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
+   
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          {mediaTab &&    <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </a>}
+         
+        
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
