bf8926d3724da2656013657ca5ca598960029667
VladyslavLata <vladlatahtc@gmail.com>
2022-12-01 17:14:36 +0200

    add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Jost } from "@next/font/google";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../constants/theme";
+
+const jost = Jost({
+  weight: ['400', '500'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <style jsx global>{`
+        body {
+          font-family: ${jost.style.fontFamily};
+        }
+      `}</style>
+    <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
