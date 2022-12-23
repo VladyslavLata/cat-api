fd6c6729bb085aac420158ee256defaf92bea077
VladyslavLata <vladlatahtc@gmail.com>
2022-12-23 18:25:47 +0200

    add component wrapp back button

--- pages/breeds/index.tsx
+++ pages/breeds/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { getCatGallery, getAllBreeds  } from "../../API/catAPI";
+import { getCatGallery, getAllBreeds } from "../../API/catAPI";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { Button } from "../../components/Button/Button";
@@ -12,19 +12,20 @@ import { selectLimit } from "../../constants/selectDatas";
 import { CurrentPage } from "../../components/CurrentPage/CurrentPage";
 import { ButtonIcon } from "../../components/ButtonIcon/ButtonIcon";
 import { Container } from "../../components/Container/Container";
+import { BackButtonWrapp } from "../../components/BackButtonWrapp/BackButtonWrapp";
 import Arrow from "../../public/arrow.svg";
-import { Box } from "../../components/Box/Box";
-import * as SC from "../../styles/Breeds.styled"; 
-
-
-
+import * as SC from "../../styles/Breeds.styled";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const param = context.query;
   try {
     const data = await Promise.all([getCatGallery(param), getAllBreeds()]);
     return {
-      props: { catsData: data[0].catsData, amountCats: data[0].amountCats , allBreeds: data[1], },
+      props: {
+        catsData: data[0].catsData,
+        amountCats: data[0].amountCats,
+        allBreeds: data[1],
+      },
     };
   } catch (error) {
     return { notFound: true };
@@ -37,11 +38,11 @@ interface IProps {
   amountCats: string;
 }
 
-const Breeds: FC<IProps> = ({ catsData, allBreeds, amountCats}) => {
+const Breeds: FC<IProps> = ({ catsData, allBreeds, amountCats }) => {
   const router = useRouter();
   const params = router.query;
   const currentPage = Number(params.page);
-// console.log(amountCats);
+  // console.log(amountCats);
   // console.log(allBreeds);
   // console.log(catsData);
 
@@ -63,37 +64,49 @@ const Breeds: FC<IProps> = ({ catsData, allBreeds, amountCats}) => {
   };
 
   const amountPage = () => {
-   return Number(amountCats) / Number(params.limit) <= currentPage + 1;
-  }
+    return Number(amountCats) / Number(params.limit) <= currentPage + 1;
+  };
 
   return (
     <>
       <Container>
-        <SC.OptionWrapp><Box display="inline-flex" gridGap="10px">
-      <ButtonIcon svg={Arrow} width={20} height={20} onClick={()=>router.back()} />
-      <CurrentPage title={"breeds"}/></Box>
-    <SC.SelectWrapp>
-      <SelectBreeds
-        optionValueDefault="allBreeds"
-        optionDefault="All breeds"
-        breeds={allBreeds}
-        name={"breed_ids"}
-        queryParam={params}
-        onChange={changeParam}
-            /></SC.SelectWrapp>
+        <SC.OptionWrapp>
+          <BackButtonWrapp>
+            <ButtonIcon
+              svg={Arrow}
+              width={20}
+              height={20}
+              onClick={() => router.back()}
+            />
+            <CurrentPage title={"breeds"} />
+          </BackButtonWrapp>
           <SC.SelectWrapp>
-      <Select
-        name={"limit"}
-        selectDatas={selectLimit}
-        queryParam={params}
-        onChange={changeParam}
-      /></SC.SelectWrapp></SC.OptionWrapp>
-      <Button onClick={() => changePage(-1)} disabled={currentPage === 0}>
-        -
-      </Button>
-      <Button onClick={() => changePage(1)} disabled={amountPage()}>+</Button>
-      <Gallery>
-        <GelleryItemBreeds dataCats={catsData} />
+            <SelectBreeds
+              optionValueDefault="allBreeds"
+              optionDefault="All breeds"
+              breeds={allBreeds}
+              name={"breed_ids"}
+              queryParam={params}
+              onChange={changeParam}
+            />
+          </SC.SelectWrapp>
+          <SC.SelectWrapp>
+            <Select
+              name={"limit"}
+              selectDatas={selectLimit}
+              queryParam={params}
+              onChange={changeParam}
+            />
+          </SC.SelectWrapp>
+        </SC.OptionWrapp>
+        <Button onClick={() => changePage(-1)} disabled={currentPage === 0}>
+          -
+        </Button>
+        <Button onClick={() => changePage(1)} disabled={amountPage()}>
+          +
+        </Button>
+        <Gallery>
+          <GelleryItemBreeds dataCats={catsData} />
         </Gallery>
       </Container>
     </>
