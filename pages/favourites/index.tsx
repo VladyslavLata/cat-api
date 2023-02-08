dab184f908eaa4040036d2a051929c2539adf58f
VladyslavLata <vladlatahtc@gmail.com>
2023-02-08 17:49:53 +0200

    settings worked btns change page on favourite page. setting displayed message

--- pages/favourites/index.tsx
+++ pages/favourites/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { getFavouritesCats } from "../../API/catAPI";
 import { IFavouriteDataCat } from "../../types/types";
 import { Message } from "../../components/Message/Message";
 import { Container } from "../../components/Container/Container";
 import { Gallery } from "../../components/Gallery/Gallery";
+import { ButtonsChangePages } from "../../components/ButtonsChangePages/ButtonsChangePages";
 import { SharedLayoutLikedPage } from "../../components/SharedLayoutLikedPage/SharedLayoutLikedPage";
 import { FavoriteCatNavigation } from "../../components/FavoriteCatNavigation/FavoriteCatNavigation";
 import { BackPagePanel } from "../../components/BackPagePanel/BackPagePanel";
@@ -38,12 +40,25 @@ interface IProps {
 }
 
 const FavouritesPage: FC<IProps> = ({ favouriteCatsData, amountCats }) => {
-  // console.log(favouriteCatsData);
-  // console.log(amountCats);
+  console.log(favouriteCatsData);
+  console.log(amountCats);
+  const router = useRouter();
+  const { query, pathname } = router;
+  const currentPage = Number(query.page);
+
+  if (favouriteCatsData.length === 0 && amountCats === "0" && currentPage > 0) {
+    router.push({
+      pathname: pathname,
+      query: {...query, page: currentPage - 1}
+  })
+}
+
   return (
     <SharedLayoutLikedPage pageName="favourites">
       {favouriteCatsData.length > 0 && <Gallery dataCats={favouriteCatsData} />}
-      {favouriteCatsData.length === 0 && <Message>No item found</Message>}
+      {favouriteCatsData.length === 0 && currentPage === 0 && <Message>No item found</Message>}
+      {Number(amountCats) > 10 && <ButtonsChangePages amountCats={amountCats} />}
+      {/* <ButtonsChangePages amountCats={amountCats}/> */}
     </SharedLayoutLikedPage>
     // <>
     //   <FavoriteCatNavigation />
